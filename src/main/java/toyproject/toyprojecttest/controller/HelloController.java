d235ef81f5c25b593f5b4b19f424395f6897a742
Hoon Kim <b0aeiou@gmail.com>
2021-01-04 18:12:39 +0900

    수정 : HelloController.java 내용 : GetMapping "hello-mvc" 추가

--- src/main/java/toyproject/toyprojecttest/controller/HelloController.java
+++ src/main/java/toyproject/toyprojecttest/controller/HelloController.java
@@ -3,6 +3,7 @@ package toyproject.toyprojecttest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "김훈");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
