3daa447fcc5617a50e95ef349bf04efb7ba00faf
Hoon Kim <b0aeiou@gmail.com>
2021-01-04 17:12:05 +0900

    추가 : Spring Boot Starter File 내용 : start.spring.io 제공

---
+++ src/main/java/toyproject/toyprojecttest/ToyprojectTestApplication.java
@@ -0,0 +1,13 @@
+package toyproject.toyprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectTestApplication.class, args);
+	}
+
+}
